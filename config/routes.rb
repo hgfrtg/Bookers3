f3770585c5c204d4ab53538b9a65020f55188062
hgfrtg <jrbevknf@outlook.com>
2019-12-23 19:55:30 +0900

    コメント機能仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only:[:new, :create, :index, :show, :destroy,:edit,:update] do
 	  resource :favorites, only: [:create, :destroy]
+	  resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only:[:new,:index,:show, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
