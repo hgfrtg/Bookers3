f4458743856cc70ca47e77ed5be12197ebd96cc2
hgfrtg <jrbevknf@outlook.com>
2019-12-26 19:43:24 +0900

    [Finish] 2ヶ月目応用課題4フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 	  resource :favorites, only: [:create, :destroy]
 	  resource :book_comments, only: [:create, :destroy]
   end
-  resources :users, only:[:new,:index,:show, :update, :edit]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :relationships, only: [:index, :create, :destroy]
+  resources :users, only:[:new,:index,:show, :update, :edit] do
+  	 member do
+     get :followings, :followers
+    end
+  end
 end
